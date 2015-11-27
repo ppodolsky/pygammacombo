103c3b66f60ba9f2afa012b03a60740f7df9a577
PashaPodolsky <ppodolsky@me.com>
2015-11-27 12:00:07 +0300

    fixed again

--- prepare_dict.py
+++ prepare_dict.py
@@ -3,15 +3,16 @@ import megazord
 
 
 dict_h = ['RooBinned2DBicubicBase.h', 'RooCrossCorPdf.h', 'RooHistPdfAngleVar.h', 'RooHistPdfVar.h', 'RooPoly3Var.h', 'RooPoly4Var.h', 'RooSlimFitResult.h', 'coreLinkDef.h']
-rcint = ['rootcint', '-cint', '-f', 'gammacombo/transResult.cxx', '-c', '-p', '-Igammacombo/include']
+rcint = ['rootcint', '-cint', '-f', 'gammacombo/gammacomboCoreDict.cxx', '-c', '-p', '-Igammacombo/include']
 megazord.system.call(*(rcint+dict_h))
-megazord.system.move('gammacombo/transResult.cxx', 'gammacombo/src/gammacomboCoreDict.cpp')
-megazord.system.move('gammacombo/transResult.h', 'gammacombo/include/gammacomboCoreDict.h')
+megazord.system.move('gammacombo/gammacomboCoreDict.cxx', 'gammacombo/src/gammacomboCoreDict.cpp')
+megazord.system.move('gammacombo/gammacomboCoreDict.h', 'gammacombo/include/gammacomboCoreDict.h')
 
 
 dict_h = ['RooAdsCartCoordVar.h', 'RooGLWADSDmixAcpADSVar.h', 'RooGLWADSDmixAcpVar.h', 'RooGLWADSDmixMaster.h',
           'RooGLWADSDmixRADSVar.h', 'RooGLWADSDmixRcpVar.h', 'RooGLWADSDmixRcpNewVar.h', 'RooGLWADSDmixRcpRatioVar.h',
           'RooGLWADSDmixRkpVar.h', 'RooGLWADSDmixRpmVar.h', 'RooGlwCartCoordVar.h', 'gammacomboLinkDef.h']
+rcint = ['rootcint', '-cint', '-f', 'gammacombo/gammacomboDict.cxx', '-c', '-p', '-Igammacombo/include']
 megazord.system.call(*(rcint+dict_h))
-megazord.system.move('gammacombo/transResult.cxx', 'gammacombo/src/gammacomboDict.cpp')
-megazord.system.move('gammacombo/transResult.h', 'gammacombo/include/gammacomboDict.h')
+megazord.system.move('gammacombo/gammacomboDict.cxx', 'gammacombo/src/gammacomboDict.cpp')
+megazord.system.move('gammacombo/gammacomboDict.h', 'gammacombo/include/gammacomboDict.h')
