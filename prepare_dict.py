07a92ed88d27cb4a1256abe1aec7f9940a4cdd92
PashaPodolsky <ppodolsky@me.com>
2016-02-05 11:00:01 +0300

    modified gammacombo

--- prepare_dict.py
+++ prepare_dict.py
@@ -2,7 +2,8 @@ import megazord
 """Generate dictionaries for code (require rootcint tool"""
 
 
-dict_h = ['RooBinned2DBicubicBase.h', 'RooCrossCorPdf.h', 'RooHistPdfAngleVar.h', 'RooHistPdfVar.h', 'RooPoly3Var.h', 'RooPoly4Var.h', 'RooSlimFitResult.h', 'coreLinkDef.h']
+dict_h = ['RooBinned2DBicubicBase.h', 'RooCrossCorPdf.h', 'RooHistPdfAngleVar.h', 'RooHistPdfVar.h',
+          'RooPoly3Var.h', 'RooPoly4Var.h', 'RooSlimFitResult.h', 'coreLinkDef.h']
 rcint = ['rootcint', '-cint', '-f', 'gammacombo/gammacomboCoreDict.cxx', '-c', '-p', '-Igammacombo/include']
 megazord.system.call(*(rcint+dict_h))
 megazord.system.move('gammacombo/gammacomboCoreDict.cxx', 'gammacombo/src/gammacomboCoreDict.cpp')
@@ -11,7 +12,8 @@ megazord.system.move('gammacombo/gammacomboCoreDict.h', 'gammacombo/include/gamm
 
 dict_h = ['RooAdsCartCoordVar.h', 'RooGLWADSDmixAcpADSVar.h', 'RooGLWADSDmixAcpVar.h', 'RooGLWADSDmixMaster.h',
           'RooGLWADSDmixRADSVar.h', 'RooGLWADSDmixRcpVar.h', 'RooGLWADSDmixRcpNewVar.h', 'RooGLWADSDmixRcpRatioVar.h',
-          'RooGLWADSDmixRkpVar.h', 'RooGLWADSDmixRpmVar.h', 'RooGlwCartCoordVar.h', 'gammacomboLinkDef.h']
+          'RooGLWADSDmixRkpVar.h', 'RooGLWADSDmixRpmVar.h', 'RooGlwCartCoordVar.h', 'RooGLWAcpVar.h', 'RooGLWRcpVar.h',
+          'RooADSAVar.h', 'RooADSRVar.h', 'RooADSRpmVar.h', 'gammacomboLinkDef.h']
 rcint = ['rootcint', '-cint', '-f', 'gammacombo/gammacomboDict.cxx', '-c', '-p', '-Igammacombo/include']
 megazord.system.call(*(rcint+dict_h))
 megazord.system.move('gammacombo/gammacomboDict.cxx', 'gammacombo/src/gammacomboDict.cpp')
