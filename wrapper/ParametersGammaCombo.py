b77334aa0cc58fcce3ee069992c06d280f17c4be
PashaPodolsky <ppodolsky@me.com>
2016-02-14 08:34:17 +0300

    fixes

--- wrapper/ParametersGammaCombo.py
+++ wrapper/ParametersGammaCombo.py
@@ -1,4 +1,6 @@
 from wrapper import *
 from wrapper.ParametersAbs import ParametersAbs
+
 ParametersGammaCombo = mod.add_class('ParametersGammaCombo', parent=ParametersAbs)
+ParametersGammaCombo.add_constructor([])
 ParametersGammaCombo.add_method('defineParameters', None, [], visibility='protected')
